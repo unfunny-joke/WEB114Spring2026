79b36700745033ec64a2d72b75c1cc3ae60ef5e6
CallieShrum <colton.shrum.2019@gmail.com>
2026-02-26 12:20:42 -0600

    Week 1

---
+++ fortune.js
@@ -0,0 +1,9 @@
+//Callie Shrum, 2/26/2026
+
+let name = "Callie";
+let career = "Database Admin";
+let location = "Denver";
+let pets = 100;
+let salary = "1 bajillion dollars";
+
+console.log(`My name is ${name} and I will be a ${career} and living in ${location}. I will have ${pets} pets and hopefully make ${salary}.`);
---
+++ me.js
@@ -0,0 +1,10 @@
+// Callie Shrum, 2/26/2026
+
+let name = "Callie";
+let favoriteColor = "Purple";
+let favoriteHobby = "Play Video Games";
+let favoriteFood = "Sushi";
+let favoriteAnimal = "Red Pandas";
+let favoriteSeason = "Fall";
+
+console.log(`Hello, my name is ${name}. Some of my favorite things are ${favoriteColor}, ${favoriteFood}, ${favoriteAnimal}, and ${favoriteSeason}. In my spare time I like to ${favoriteHobby}.`);
